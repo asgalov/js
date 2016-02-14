4c86b9fb58159b551a83a05756080724fac7c051
asgalov <asgalov@gmail.com>
2016-02-14 22:15:06 +0400

    new functionality

--- CleverTyper/main.js
+++ CleverTyper/main.js
@@ -1,34 +1,33 @@
 function checkTyping(){
-  var check_area = document.getElementById("check_area");  
-  console.log(check_area.value); 
-  var vocabulary = document.getElementById("vocabulary");  
-  var str =  vocabulary.value;
-  var index =  vocabulary.currentLetterIndex;
-  console.log(str[index]); 
+    var check_area = document.getElementById("check_area");  
+    console.log(check_area.value); 
+    var vocabulary = document.getElementById("vocabulary");  
+    var str =  vocabulary.value;
+    var index =  vocabulary.currentLetterIndex;
+    console.log(str[index]); 
 }
 
 window.onload = function(){
-  var vocabulary = document.getElementById("vocabulary");  
-  vocabulary.value = "new text to type";
-  vocabulary.currentLetterIndex = 0;
+    var vocabulary = document.getElementById("vocabulary");  
+    vocabulary.value = "new text to type";
+    vocabulary.currentLetterIndex = 0;
 };
 
-//document.onkeypress = function(e){
 document.onkeydown = function(e){
-  var keyCode = e.keyCode;
-  var vocabulary = document.getElementById("vocabulary");  
-  var str =  vocabulary.value;
-  var index = vocabulary.currentLetterIndex;
-  var id = keyCode.toString(); 
-  console.log(str[index]+" "+id+" "+String.fromCharCode(keyCode)); 
-  if (str[index] == String.fromCharCode(keyCode)){
-    vocabulary.currentLetterIndex += 1;
-  } else {
-    console.log("aaaaaaaaaaaaaaaaaaxxi");
-    event.preventDefault();
-    var key = document.getElementById(id);  
-    key.style.background="red";
-  }
+    var keyCode = e.keyCode;
+    var vocabulary = document.getElementById("vocabulary");  
+    var str =  vocabulary.value;
+    var index = vocabulary.currentLetterIndex;
+    var id = keyCode.toString(); 
+    console.log(str[index]+" "+id+" "+String.fromCharCode(keyCode)+
+        +" "+ vocabulary.value.charCodeAt(index)); 
+    if (str[index] == (String.fromCharCode(keyCode).toLowerCase())){
+        vocabulary.currentLetterIndex += 1;
+    } else {
+        event.preventDefault();
+        var key = document.getElementById(id);  
+        key.style.background="red";
+    }
 };
   
 document.onkeyup= function(e){
