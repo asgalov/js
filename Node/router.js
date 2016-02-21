f61865a1981ac4d1c78b54abf0fc5feeb85f0241
asgalov <asgalov@gmail.com>
2016-02-22 00:35:30 +0400

    new logic

--- Node/router.js
+++ Node/router.js
@@ -1,5 +1,12 @@
-function route(pathname){
-  console.log("route request for "+pathname);
+function route(pathname, handler, response){
+    if (typeof handler[pathname] === 'function'){
+        handler[pathname](response);
+    } else{
+        console.log("No request handler found for " + pathname);
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("No request handler found ");
+        response.end();
+    }
 }
 
 exports.route = route 
--- Node/server.js
+++ Node/server.js
@@ -2,17 +2,17 @@ var http = require("http");
 var url = require("url");
 console.log("hello world!");
 
-function start(route){
+function start(route, handler){
+
     function onRequest(request, response){
         console.log("response received!");
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
-        route(pathname);
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World");
-        response.end();
+        route(pathname, handler, response);
     }
+
     http.createServer(onRequest).listen(8000);
+
     console.log("server just started!");
 }
 
